7543f7ef06f63ac63216cc0c438b21a2b4f12590
Peter Son <peteson.me@gmail.com>
2021-04-02 11:16:20 +1300

    create shopify token

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+import { client } from '../utils/shopify-client'
+
 export default function Home() {
   return (
     <div className={styles.container}>
